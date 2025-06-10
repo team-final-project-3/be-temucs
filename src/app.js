b5bcef217ef94745108c58dcccbf0a4cd17c6cc5
Jason Wijaya <jasonwijaya2@gmail.com>
2025-06-10 17:00:17 +0700

    add authentication

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const authMiddleware = require("./middlewares/auth");
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
 const dummyRoutes = require("./routes/dummy.routes");
@@ -9,7 +10,14 @@ const swaggerSpec = require("./configs/swagger");
 const app = express();
 
 app.use(express.json());
-app.use("/api", userRoutes, adminRoutes, dummyRoutes, branchRoutes);
+app.use(
+  "/api",
+  authMiddleware,
+  userRoutes,
+  adminRoutes,
+  dummyRoutes,
+  branchRoutes
+);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
---
+++ src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const unprotectedRoutes = ["/api/register", "/api/login"];
+
+module.exports = (req, res, next) => {
+  // Allow unprotected routes
+  if (unprotectedRoutes.includes(req.path)) {
+    return next();
+  }
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.user = decoded;
+    next();
+  });
+};
