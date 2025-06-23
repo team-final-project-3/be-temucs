171af237bbe6ea0899bd28698b6f314cd00117a9
Jason Wijaya <jasonwijaya2@gmail.com>
2025-06-23 09:30:53 +0700

    fix verifyUserToken at saveExpoToken routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 const { allowRoles } = require("../middlewares/auth");
+const { verifyUserToken } = require("../auth/user.auth");
 
 /**
  * @swagger
